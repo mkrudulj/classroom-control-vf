af15eacc0fede2a6eef43173a5b8f0346b8a3438
mkrudulj <mkrudulj@gmail.com>
2016-06-09 12:00:50 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,10 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  if $::virtual != 'physical' {
+    $vmname = capitalize($::virtual)
+    notify { "This is a ${vmname} virtual machine.": }
+  }
   notify { "Hello, my name is ${::hostname}": }
   exec { 'update /etc/motd':
   path => '/usr/local/bin',
